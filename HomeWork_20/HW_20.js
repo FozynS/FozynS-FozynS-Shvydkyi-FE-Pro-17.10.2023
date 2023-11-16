eef5a8087ad8ce5f3039376d17aa4f98b29755d0
Danylo Shvydkyi <wolf44488@gmail.com>
2023-11-16 16:59:05 +0100

    HomeWork 20

---
+++ HomeWork_20/HW_20.js
@@ -0,0 +1,21 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this
+    },
+    showStep: function () { // показывает текущую ступеньку
+        console.log(this.step);
+    }
+};
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+
+ladder.up().up().down().showStep(); // 1
