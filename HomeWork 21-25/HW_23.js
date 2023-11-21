de116661fd7cf6a952458931ff0062dc4f899d46
Danylo Shvydkyi <wolf44488@gmail.com>
2023-11-21 10:47:56 +0100

    Retake HomeWork again

--- HomeWork 21-25/HW_23.js
+++ HomeWork 21-25/HW_23.js
@@ -1,19 +1,3 @@
-/*
-Вам потрібно зробити конструктор сутності "Студент".
-Студент має ім'я, прізвище, рік народження — це властивості. Є масив з оцінками, це також властивість. 
-І є можливість отримати вік студента та його середній бал – це методи.
-
-Ще у всіх Студентів є по масиву однакової довжини, у ньому 25 елементів, спочатку він не заповнений, але на 25 елементів. 
-Це масив, в якому відзначається відвідуваність, щоразу коли ми викликаємо метод .present() на чергове порожнє місце, 
-    в масив записується true, коли викликаємо .absent() - записується false. 
-Передбачте будь-який захист від того, щоб у масиві відвідуваності не могло бути більше 25 записів.
-Масив – це властивість, present та absent – ​​методи.
-Останній метод: .summary(), перевіряє середню оцінку і середнє відвідування(кількістьВідвідин/кількістьЗанять), 
-    і якщо середня оцінка більше 90, а середнє відвідування більше 0.9, то метод summary повертає рядок "Молодець!", 
-    якщо одне з цих значень менше , то - "Добре, але можна краще ", якщо обидва нижче - "Редиска!".
-Не забудьте після того, як напишите цей конструктор, створити 2-3 екземпляри (конкретних студентів) і показати використання цих 
-    методів.
-*/ 
 class Student {
     constructor(name, surname, birthyear, marks) {
         this.name = name;
@@ -21,33 +5,24 @@ class Student {
         this.birthyear = birthyear;
         this.marks = marks;
         this.attendance = new Array(25).fill(null);
-        // this.numberOfClasses = 0;
-        // this.attendance = [];
-        // this.attendance.length = 25;
     }
 
-    // attendanceCheck() {
-        
-    // }
-
-    present() {
+    attendanceCheck(status) {
         const emptyIndex = this.attendance.indexOf(null);
         if(emptyIndex !== -1) {
-            this.attendance[emptyIndex] = true;
+            this.attendance[emptyIndex] = status;
         } else {
             console.log('There are no more spaces available to mark attendance.')
         }
         return this;
+    }
+
+    present() {
+        return this.attendanceCheck(true);
     }
 
     absent() {
-        const emptyIndex = this.attendance.indexOf(null);
-        if(emptyIndex !== -1) {
-            this.attendance[emptyIndex] = false;
-        } else {
-            console.log('There are no more spaces available to mark attendance.')
-        }
-        return this;
+        return this.attendanceCheck(false);
     }
 
     numberOfClasses() {
@@ -62,14 +37,13 @@ class Student {
 
     averageScore() {
         const score = Math.floor(this.marks.reduce((total, value) => total + value, 0) / this.marks.length);
-        console.log(score);
         return score;
     }
 
     summary() {
         const allTrue = this.attendance.filter((item) => item === true);
         const averageAttendance = (allTrue.length / this.numberOfClasses()).toFixed(2);
-        // const roundAverageAttendance = Math.floor(averageAttendance * 100) / 100;
+
         const pointAverage = 9;
         const validAttendance = 0.9;
         const score = this.averageScore();
@@ -91,6 +65,7 @@ class Student {
 const danylo = new Student('Danylo', 'Shvydkyi', 2001, [12, 10, 11, 10, 9, 9, 7, 10, 11, 12, 10, 11, 11]);
 const nikita = new Student('Nikita', 'Shabelskiy', 2005, [12, 10, 11, 10, 12, 12, 11, 10, 11, 12, 11, 12, 12]);
 const bogdan = new Student('Bogdan', 'Shvydkyi', 1994, [12, 12, 9, 8, 7, 6, 7, 6, 8, 9, 6, 5, 8]);
+
 danylo.present().present().present().present().present().present().present().absent().present();
 danylo.summary();
 
--- HomeWork 21-25/HW_24.js
+++ HomeWork 21-25/HW_24.js
@@ -1,23 +1,8 @@
-/*
-Мережа фастфудів пропонує кілька видів гамбургерів:
-    маленький (50 тугриків, 20 калорій);
-    великий (100 тугриків, 40 калорій).
-Гамбургер може бути з одним із декількох видів начинок:
-    сиром (+ 10 тугриків, + 20 калорій);
-    салатом (+ 20 тугриків, + 5 калорій);
-    картоплею (+ 15 тугриків, + 10 калорій).
-Можна додати добавки:
-    посипати приправою (+15 тугриків, 0 калорій) - полити майонезом (+ 20 тугриків, +5 калорій).
-    Напишіть програму, яка розраховує вартість та калорійність гамбургера. Використовуйте ООП підхід.
-(підказка: потрібен клас Гамбургер, константи, методи для вибору опцій та розрахунку потрібних величин)
-*/
 class Hamburger{
     constructor(size, stuffing) {
         this.size = size;
         this.stuffing = stuffing;
         this.toppings = [];
-        this.price = 0;
-        this.calories = 0;
     }
     static SIZE_SMALL = {price: 50, calories: 20};
     static SIZE_BIG = {price: 100, calories: 40};
@@ -30,31 +15,31 @@ class Hamburger{
     static TOPPING_MAYO = {price: 20, calories: 5};
 
     addTopping(topping) {
-        if(topping === Hamburger.TOPPING_SAUCE || topping === Hamburger.TOPPING_MAYO) {
-            this.toppings.push(topping); 
-            this.price += topping.price;
-            this.calories += topping.calories;
-        } else {
-            console.log('Write topping, not stuffing');
-        }
-        
+        this.toppings.push(topping); 
     }
-
+    
+    calculateTotal(atribute, array) {
+        let total = 0;
+        for(let i = 0; i < array.length; i++){
+            total += array[i][atribute];
+        }
+        return total;
+    }
     calculateCalories() {
-        return this.calories + this.size.calories + this.stuffing.calories;
+        return this.calculateTotal('calories', this.toppings) + this.size.calories + this.stuffing.calories;
     }
 
     calculatePrice() {
-        return this.price + this.size.price + this.stuffing.price;
+        return this.calculateTotal('price', this.toppings) + this.size.price + this.stuffing.price;
     }
 }
 // маленький гамбургер з начинкою з сиру
 let bigHamburger = new Hamburger(Hamburger.SIZE_BIG, Hamburger.STUFFING_CHEESE);
-console.log(bigHamburger)
+// console.log(bigHamburger)
 
 // добавка з майонезу
 bigHamburger.addTopping(Hamburger.TOPPING_MAYO);
-console.log(bigHamburger)
+// console.log(bigHamburger)
 
 // запитаємо скільки там калорій
 console.log("Calories: " + bigHamburger.calculateCalories());
@@ -67,4 +52,4 @@ bigHamburger.addTopping(Hamburger.TOPPING_SAUCE);
 
 // А скільки тепер коштує?
 console.log("Price with sauce: " + bigHamburger.calculatePrice());
-console.log(bigHamburger)
+// console.log(bigHamburger);
